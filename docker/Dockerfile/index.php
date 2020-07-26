ebdbc2eb1786f308e3a0bf8ed51f9147389ece58
metfan1981 <korotckijdenis999@gmail.com>
2020-07-26 18:03:32 +0300

    --no-cache deleted

--- docker/Dockerfile/index.php
+++ docker/Dockerfile/index.php
@@ -2,7 +2,7 @@
 $WEB_MSG = getenv('WEB_MSG');
 $WEB_MSG_2 = getenv('WEB_MSG_2');
 
-echo "<h1>Hello! Testing Jenkins pipeline..</h1><br>";
+echo "<h1>Hello! This is Jenkins pipeline test</h1><br>";
 echo "$WEB_MSG", "<br><p>";
 echo "$WEB_MSG_2", "<br><p>";
 
