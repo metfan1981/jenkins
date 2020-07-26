e3d0167d9cb1e845973d5679927d9301c4d686f7
metfan1981 <korotckijdenis999@gmail.com>
2020-07-26 17:49:35 +0300

    added new web message

--- docker/Dockerfile/index.php
+++ docker/Dockerfile/index.php
@@ -1,8 +1,10 @@
 <?php 
 $WEB_MSG = getenv('WEB_MSG');
+$WEB_MSG_2 = getenv('WEB_MSG_2');
 
 echo "<h1>Hello! Testing Jenkins pipeline..</h1><br>";
 echo "$WEB_MSG", "<br><p>";
+echo "$WEB_MSG_2", "<br><p>";
 
 
 echo "Made by <font color=blue> Denis";
