f97bfdfe8b27ffa9f249c5ef71700b11c82e1c3e
metfan1981 <korotckijdenis999@gmail.com>
2020-07-26 16:54:38 +0300

    edit /docker/Dockerfile/index.php

--- docker/Dockerfile/index.php
+++ docker/Dockerfile/index.php
@@ -1,9 +1,9 @@
 <?php 
 $WEB_MSG = getenv('WEB_MSG');
 
-echo "<h1>Hello!</h1><br>";
+echo "<h1>Hello! Testing Jenkins here</h1><br>";
 echo "$WEB_MSG", "<br><p>";
+
 echo "Made by <font color=blue> Denis";
 
 ?>
-
